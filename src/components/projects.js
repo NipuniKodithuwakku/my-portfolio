b2f70346df37234d74d23976370faddfda521872
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-01-12 16:35:39 -0800

    added project categories

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,168 @@
 import React,{Component} from 'react';
-import {Tabs,Tab} from 'react-mdl';
+import {Tabs,Tab,Grid,Cell,Card,CardTitle,CardText,CardActions,Button,CardMenu,IconButton } from 'react-mdl';
 
 class Project extends Component{
-    constructor(props){
+    constructor(props){ 
         super(props);
         this.state = {activeTab:0}
+    }
+
+    toggleCategories(){
+        if (this.state.activeTab === 0) {
+            return(
+                <div className="projects-grid">
+
+                {/* // first project */}
+                    <Card shadow={0} style={{width: '400px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn8ayRrGHGyswY1Hx_R1iO53IYgS2ljs28pR4xbYkI-0rO0OBJ&s) center / cover'}}>recipe app</CardTitle>
+                    <CardText>
+                        using this app you can easily get your food recipes instantly
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                    </Card>
+
+                {/* //Second project */}
+                   <Card shadow={0} style={{width: '400px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn8ayRrGHGyswY1Hx_R1iO53IYgS2ljs28pR4xbYkI-0rO0OBJ&s) center / cover'}}>recipe app</CardTitle>
+                    <CardText>
+                        using this app you can easily get your food recipes instantly
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                    </Card>
+
+                    {/* Third project */}
+
+                    <Card shadow={0} style={{width: '400px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn8ayRrGHGyswY1Hx_R1iO53IYgS2ljs28pR4xbYkI-0rO0OBJ&s) center / cover'}}>recipe app</CardTitle>
+                    <CardText>
+                        using this app you can easily get your food recipes instantly
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                    </Card>
+                    </div>
+            )
+
+                
+        
+        }else if(this.state.activeTab === 1){
+            return(
+            <div className="projects-grid">
+
+                {/* // first project */}
+                    <Card shadow={0} style={{width: '400px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://i.blogs.es/77220b/original/450_1000.png) center / cover'}}>recipe app</CardTitle>
+                    <CardText>
+                        using this app you can easily get your food recipes instantly
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                    </Card>
+
+                {/* //Second project */}
+                   <Card shadow={0} style={{width: '400px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://i.blogs.es/77220b/original/450_1000.png) center / cover'}}>recipe app</CardTitle>
+                    <CardText>
+                        using this app you can easily get your food recipes instantly
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                    </Card>
+
+                    {/* Third project */}
+
+                    <Card shadow={0} style={{width: '400px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://i.blogs.es/77220b/original/450_1000.png) center / cover'}}>recipe app</CardTitle>
+                    <CardText>
+                        using this app you can easily get your food recipes instantly
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                    </Card>
+                    </div>
+            )
+
+            
+        }else if(this.state.activeTab === 2){
+            return(
+            <div className="projects-grid">
+
+                {/* // first project */}
+                    <Card shadow={0} style={{width: '400px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.silios.gr/wp-content/uploads/2019/06/flutter-1024x486.png) center / cover'}}>recipe app</CardTitle>
+                    <CardText>
+                        using this app you can easily get your food recipes instantly
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                    </Card>
+
+                {/* //Second project */}
+                   <Card shadow={0} style={{width: '400px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.silios.gr/wp-content/uploads/2019/06/flutter-1024x486.png) center / cover'}}>recipe app</CardTitle>
+                    <CardText>
+                        using this app you can easily get your food recipes instantly
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                    </Card>
+
+                    {/* Third project */}
+
+                    <Card shadow={0} style={{width: '400px', margin: 'auto'}}>
+                    <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://www.silios.gr/wp-content/uploads/2019/06/flutter-1024x486.png) center / cover'}}>recipe app</CardTitle>
+                    <CardText>
+                        using this app you can easily get your food recipes instantly
+                    </CardText>
+                    <CardActions border>
+                        <Button colored>Github</Button>
+                    </CardActions>
+                    <CardMenu style={{color: '#fff'}}>
+                        <IconButton name="share" />
+                    </CardMenu>
+                    </Card>
+                    </div>
+            )
+            
+        }else if(this.state.activeTab === 3){
+            return(<div>This is React Native</div>)
+        }else{
+            return(<div>This is MongoDB</div>)
+        }
     }
     render(){
         return (
@@ -17,7 +175,15 @@ class Project extends Component{
                     <Tab>MongoDB</Tab>
                 </Tabs>
                 <section>
-                    <div className="content">Content for the tab: {this.state.activeTab}</div>
+                    <Grid >
+                        <Cell col={12}>
+                        <div className="content">
+                            {this.toggleCategories()}
+                        
+                        </div>
+                        </Cell>
+                    
+                    </Grid>
                 </section>
 
             
