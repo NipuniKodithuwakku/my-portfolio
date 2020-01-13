b115d995bae9eaf0a9aa28d3ec479d153070e202
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-01-12 17:27:48 -0800

    added contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,21 @@
 import React,{Component} from 'react';
+import {Grid,Cell} from 'react-mdl';
 
 class Contact extends Component{
     render(){
         return (
-            <div><h1>My contact</h1></div>
+            <div className="contact-body">
+            <Grid className="contact-grid">
+                <Cell col={6}>
+                <h1>Nipuni Kodithuwakku</h1>
+                <img src="./contact.jpg" alt="contact me" style={{width:'250px',height:'250px'}}/>
+                
+                </Cell>
+                <Cell col={6}>half</Cell>
+            </Grid>
+            </div>
+           
+           
         )
     }
 }
