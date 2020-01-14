20dbb512ba53eaed7ed8f1ff61a986a9f6489cac
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-01-14 00:15:24 -0800

    modified contact list

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {Grid,Cell} from 'react-mdl';
+import {Grid,Cell,List,ListItem,ListItemContent} from 'react-mdl';
 
 class Contact extends Component{
     render(){
@@ -7,18 +7,33 @@ class Contact extends Component{
             <div className="contact-body">
             <Grid className="contact-grid">
                 <Cell col={6}>
-                <h1>Nipuni Kodithuwakku</h1>
+                <h2 style={{color:'purple'}}>Nipuni Kodithuwakku</h2>
                 <img src="./contact.jpg" alt="contact me" style={{width:'250px',height:'250px'}}/>
+                <p style={{width:'75%',fontSize:'15px',margin:'auto',paddingTop:'1em'}}>I am a level 3 Software Engineering Undergraduate university of Kelaniya. I have a good capability of following areas. Those are web designing and mobile application development. </p>
                 
                 </Cell>
                 <Cell col={6}>
-                    <p><h3><i className="fa fa-phone-square" aria-hidden="true"></i>Telephone<br/>0763988752</h3></p>
-                    <p><h3><i className="fa fa-envelope" aria-hidden="true"></i>Email<br/>chandulakan123@gmail.com</h3></p>
-                    <p><h3><i className="fa fa-whatsapp" aria-hidden="true"></i>whatsapp</h3></p>
-                    <p><h3><i className="fa fa-commenting-o" aria-hidden="true"></i>Message</h3></p>
-
-                    
-                    
+                <h2>Contact me</h2>
+                <hr/>
+                   
+                <div className='contact-list'>
+                                    <List>
+  <ListItem>
+    <ListItemContent style={{fontSize:'25px',fontFamily:'Arial'}}><i className="fa fa-phone-square" aria-hidden="true"></i>0763988752</ListItemContent>
+  </ListItem>
+  <ListItem>
+    <ListItemContent style={{fontSize:'25px',fontFamily:'Arial'}} ><i className="fa fa-envelope" aria-hidden="true"></i>chandulakan123@gmail.com</ListItemContent>
+  </ListItem>
+  <ListItem>
+    <ListItemContent style={{fontSize:'20px',fontFamily:'Arial'}} ><i className="fa fa-address-card" aria-hidden="true"></i>K.A.N chandula,<br/>Mahahena,<br/>Pallattara East,<br/>Ihala Beligalla</ListItemContent>
+  </ListItem>
+  
+  
+</List>
+                 
+                
+                </div>
+       
                 
                 </Cell>
             </Grid>
